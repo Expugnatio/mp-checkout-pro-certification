92aa3e959caf44a77a05ba1eca6b2facca364ac7
Expugnatio <christiam.alejandro.hernandez@gmail.com>
2021-07-08 17:21:00 -0500

    :wrench: Fix webhook

--- src/main/java/com/mp/checkoutpro/web/WebhookController.java
+++ src/main/java/com/mp/checkoutpro/web/WebhookController.java
@@ -1,19 +1,40 @@
 package com.mp.checkoutpro.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Scanner;
+
+@RestController
 @RequestMapping("/webhook")
-@RequiredArgsConstructor
 public class WebhookController {
 
     @PostMapping
-    public String viewProducts(@RequestParam("payment_id") String paymentId, @RequestParam("status") String status,
-            @RequestParam("external_reference") String externalReference, @RequestParam("merchant_order_id") String merchantOrderId) {
-        return "main";
+    public ResponseEntity receiveNotification(HttpServletRequest request) {
+        String notificationContent = extractPostRequestBody(request);
+        System.out.println(notificationContent);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Notification Received");
+    }
+
+    static String extractPostRequestBody(HttpServletRequest request) {
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            Scanner s = null;
+            try {
+                s = new Scanner(request.getInputStream(), "UTF-8").useDelimiter("\\A");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return s.hasNext() ? s.next() : "";
+        }
+        return "";
     }
 }
